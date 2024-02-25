0a65ea90269a39766b62efbde9a494f7a7fcbb89
OnrrGvnn <158863872+OnrrGvnn@users.noreply.github.com>
2024-02-25 11:44:56 +0300

    lesson6 completed

---
+++ DayOne/javademos/variables/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //case sensitive - büyük küçük harf
+        int ogrenciSayisi = 10;
+        String mesaj = "Öğrenci sayısı : ";
+
+        System.out.println(mesaj +ogrenciSayisi);
+        System.out.println(mesaj +ogrenciSayisi);
+        System.out.println(mesaj +ogrenciSayisi);
+        System.out.println(mesaj +ogrenciSayisi);
+
+    }
+}
