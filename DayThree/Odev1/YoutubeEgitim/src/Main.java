1051e82613d0dd350686c7c41fe75c8184b7a3fb
OnrrGvnn <158863872+OnrrGvnn@users.noreply.github.com>
2024-02-28 13:23:49 +0300

    Day3 Odev1 completed

---
+++ DayThree/Odev1/YoutubeEgitim/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        CustomerManager customerManager = new CustomerManager(new Customer(), new MilitaryCreditManager());
+        customerManager.creditManager.Save();
+
+
+        Customer customer1 = new Customer();
+        customer1.id = 1;
+        customer1.Name = "Onur";
+
+    }
+}
---
+++ DayThree/Odev1/YoutubeEgitim/src/TeacherCreditManager.java
@@ -0,0 +1,7 @@
+public class TeacherCreditManager extends BaseCreditManager implements ICreditManager {
+    @Override
+    public void Calculate() {
+        System.out.println("Ogretmen Kredisi Hesaplandı");
+    }
+
+}
